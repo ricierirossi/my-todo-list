086cafa580ae31ab45f6435964ff54ea0d5fd92e
Ricieri <matheus.ricieri@hotmail.com>
2023-10-30 16:30:16 -0300

    Add fontawesome

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
-import './assets/main.css'
+import "./assets/main.css";
 
 import { createPinia } from "pinia";
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).use(createPinia()).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faSpinner);
+
+createApp(App)
+    .use(createPinia())
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .mount("#app");

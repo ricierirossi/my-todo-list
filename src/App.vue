c7222861354889c968da1c95e191ac69c322f965
Ricieri <matheus.ricieri@hotmail.com>
2023-10-30 16:29:04 -0300

    Add background color

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
     <div>
         <div class="h-auto flex flex-col gap-y-5 bg-dark text-ice py-5">
-            <div class="flex justify-center gap-1 text-3xl">
-                <span class="material-symbols-outlined text-3xl"> lists </span>
+            <div class="flex justify-center gap-1 text-3xl bg-dark">
+                <span class="material-symbols-outlined text-3xl bg-dark">
+                    lists
+                </span>
                 Tarefas
             </div>
-            <TaskForm />
+            <TaskForm class="bg-dark" />
         </div>
-        <div class="flex justify-center pt-3 bg-lead">
+        <div class="flex justify-center pt-3 bg-lead h-96">
             <div class="flex flex-col gap-3 items-center w-96 bg-lead">
-                <div class="flex justify-between items-center w-full">
-                    <div class="text-ice" v-if="filter == 'all'">
+                <div class="flex justify-between items-center w-full bg-lead">
+                    <div class="text-ice bg-lead" v-if="filter == 'all'">
                         Você possui {{ tasksStore.totalTasks }} tarefa(s)
                     </div>
-                    <div class="text-ice" v-else>
+                    <div class="text-ice bg-lead" v-else>
                         Você possui {{ tasksStore.totalFavsTasks }} tarefa(s)
                         favoritas
                     </div>
-                    <div class="flex gap-2 self-end">
+                    <div class="flex gap-2 self-end bg-lead">
                         <TaskButtons
                             label="Todas"
                             @click="[(filter = 'all'), tasksStore.filterAll()]"

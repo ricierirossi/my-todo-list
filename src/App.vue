168535c3bef3506f1209dd198ae4ee8ae1fa0df5
Chopper <matheus.ricieri@hotmail.com>
2023-11-16 13:08:31 -0300

    Add feat bottom form on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="">
+    <div class="flex flex-col min-h-screen">
         <div class="bg-dark text-ice flex flex-col gap-y-4">
             <div class="flex justify-center gap-x-2 items-end mt-1">
                 <span class="material-symbols-outlined text-6xl"> lists </span>
                 <span class="text-7xl">Tarefas</span>
             </div>
-            <TaskForm />
+            <TaskForm v-if="!bottomForm.value" />
         </div>
         <div>
             <div>
@@ -33,17 +33,19 @@
                 <TaskCards :filter="filter" class="flex justify-center" />
             </div>
         </div>
-        <footer class="relative top-full text-center py-5">
+
+        <footer class="mt-auto text-center w-full py-3">
             <span>Desenvolvido por </span>
             <a href="https://github.com/ricierirossi" class="text-pineapple"
                 >ricieri</a
             >
         </footer>
+        <TaskForm v-if="bottomForm.value" />
     </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, onBeforeUnmount, onMounted, onBeforeMount } from "vue";
 import TaskForm from "./components/TaskForm.vue";
 import TaskButtons from "./components/TaskButtons.vue";
 import TaskCards from "./components/TaskCards.vue";
@@ -53,4 +55,27 @@ const tasksStore = useTasksStore();
 tasksStore.getTasks();
 
 const filter = ref("all");
+const windowWidth = ref(window.innerWidth);
+const bottomForm = ref("");
+
+const handleResize = () => {
+    windowWidth.value = window.innerWidth;
+    window.innerWidth <= 420
+        ? (bottomForm.value = ref(true))
+        : (bottomForm.value = ref(false));
+};
+
+onBeforeMount(() => {
+    window.innerWidth <= 420
+        ? (bottomForm.value = ref(true))
+        : (bottomForm.value = ref(false));
+});
+
+onMounted(() => {
+    window.addEventListener("resize", handleResize);
+});
+
+onBeforeUnmount(() => {
+    window.removeEventListener("resize", handleResize);
+});
 </script>

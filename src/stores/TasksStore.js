3a0fcad7f84c89242eb8e2f769ce677496f58a25
Chopper <matheus.ricieri@hotmail.com>
2023-11-17 16:35:29 -0300

    Add pthers HTTP request firebase URL
    Change all HTTP requests from json server into firebase URL, use id as the one geneated by firebase

--- src/stores/TasksStore.js
+++ src/stores/TasksStore.js
@@ -8,20 +8,23 @@ export const useTasksStore = defineStore("tasksStore", {
     actions: {
         async getTasks() {
             this.loading = true;
-            // await fetch("http://localhost:3000/tasks")
+
             await fetch(
                 "https://my-todo-list-7555f-default-rtdb.firebaseio.com/tasks.json"
             )
                 .then((resp) => resp.json())
-                // .then((data) => (this.tasks = data));
+
                 .then((data) => {
                     this.tasks = Object.keys(data).map((key) => data[key]);
+
+                    this.tasks.forEach(
+                        (task, index) => (task.id = Object.keys(data)[index])
+                    );
                 });
             this.loading = false;
         },
         async addTask(task) {
             this.tasks.push(task);
-            // await fetch("http://localhost:3000/tasks", {
             await fetch(
                 `https://my-todo-list-7555f-default-rtdb.firebaseio.com/tasks.json`,
                 {
@@ -34,9 +37,8 @@ export const useTasksStore = defineStore("tasksStore", {
         async favTask(id) {
             const task = this.tasks.find((t) => t.id === id);
             task.isFav = !task.isFav;
-            console.log(Object.keys(data));
             await fetch(
-                `https://my-todo-list-7555f-default-rtdb.firebaseio.com/tasks/${name}.json`,
+                `https://my-todo-list-7555f-default-rtdb.firebaseio.com/tasks/${id}.json`,
                 {
                     method: "PATCH",
                     body: JSON.stringify({ isFav: task.isFav }),
@@ -44,20 +46,23 @@ export const useTasksStore = defineStore("tasksStore", {
                 }
             );
         },
-        // async deleteTask(id) {
-        //     this.tasks = this.tasks.filter((t) => {
-        //         return t.id != id;
-        //     });
-        //     await fetch(`http://localhost:3000/tasks/${id}`, {
-        //         method: "DELETE",
-        //     });
-        // },
-        // filterAll() {
-        //     this.getTasks();
-        // },
-        // filterFavs() {
-        //     this.tasks = this.tasks.filter((t) => t.isFav);
-        // },
+        async deleteTask(id) {
+            this.tasks = this.tasks.filter((t) => {
+                return t.id != id;
+            });
+            await fetch(
+                `https://my-todo-list-7555f-default-rtdb.firebaseio.com/tasks/${id}.json`,
+                {
+                    method: "DELETE",
+                }
+            );
+        },
+        filterAll() {
+            this.getTasks();
+        },
+        filterFavs() {
+            this.tasks = this.tasks.filter((t) => t.isFav);
+        },
     },
     getters: {
         totalTasks() {
